e6f723ac2caccf589d98cc47690aa7cfcd0e27fd
SartMans <sartman@hotmail.com>
2023-12-06 00:30:14 -0500

    first commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import Cart from "./Cart";
+import Footer from "./Footer";
+import Product from "./Product";
+import HeroBanner from "./HeroBanner";
+import FooterBanner from "./FooterBanner";
+import Layout from './Layout';
+import Navbar from './Navbar';
+
+export{
+    Cart,
+    Footer,
+    FooterBanner,
+    HeroBanner,
+    Product,
+    Layout,
+    Navbar,
+}
+
---
+++ ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'pmrrk7m7',
+    dataset: 'production'
+  }
+})
